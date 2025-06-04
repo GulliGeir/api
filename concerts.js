e847a1985781156d01f045cd5144543cacbfc9b9
GulliGeir <79102205+GulliGeir@users.noreply.github.com>
2025-06-04 17:04:57 +0000

    Update concerts.js

--- concerts.js
+++ concerts.js
@@ -1,6 +1,11 @@
 export default async function handler(req, res) {
-  const response = await fetch('http://apis.is/concerts');
-  const data = await response.json();
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json(data);
+  try {
+    const response = await fetch('http://apis.is/concerts');
+    const data = await response.json();
+    // Allow any domain to access this endpoint (for browser CORS)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch concerts' });
+  }
 }
